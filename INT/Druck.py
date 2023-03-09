0a5f33f28ae75bb95b623c433b25f2022e1cd05e
Jascha Fricker <frickerjascha@gmail.com>
2023-03-09 17:06:38 +0100

    ölk

--- INT/Druck.py
+++ INT/Druck.py
@@ -11,17 +11,17 @@ import matplotlib.ticker as ticker
 
 
 
-Y_LABEL = r"Zählrate"
-X_LABEL = r"Stromstärke in $mA$"
+Y_LABEL = r"Druck $\Delta p$ in Mpa"
+X_LABEL = r"Brechungsindex $\Delta n$"
 X_START =0 
 Y_START =0
 X_END = 2.5e-7
-Y_END = 2200
+Y_END = 120000
 
-X_MAJOR_TICK = 0.2 *0.001
-Y_MAJOR_TICK =500
-X_MINOR_TICK = 0.05 *0.001
-Y_MINOR_TICK = 100
+X_MAJOR_TICK = 0.5e-7
+Y_MAJOR_TICK =20000
+X_MINOR_TICK = 0.1e-7
+Y_MINOR_TICK = 5000
 SAVE_AS = "./INT/plots/druck.pdf"
 
 path_ = "./INT/INT.xls"
@@ -45,8 +45,8 @@ fig, ax = plt.subplots()
 ax.grid()
 data[0] = [cal_N(i) for i in data[0]]
 
-data.append([(analogErr(0.02)+i*0.016)*1e-5 for i in data[1]])
-data[1] = [(1.0135-i)*1e-5 for i in data[1]]
+data.append([(analogErr(0.02)+i*0.016)*1e5 for i in data[1]])
+data[1] = [(1.0135-i)*1e5 for i in data[1]]
 #print(data)
 
 #---------------  fit
@@ -57,14 +57,34 @@ def funcArr(n,arr):
     return func(n,arr[0],arr[1])
 
 
-popt,perr = optimize.curve_fit(func,data[0],data[1])
+popt,perr = optimize.curve_fit(func,data[0],data[1],p0=[1e-12,0])
 print(popt,np.sqrt(np.diag(perr)))
 fitDat =genDataFromFunktion(1000,X_START,X_END,popt,funcArr)
 
 
 #ax.scatter(data[0],data[1],s=15)
-err2 =ax.errorbar(data[0],data[1],fmt="x",yerr=data[2], ecolor = 'black',elinewidth=0.9,capsize=4,capthick=0.9)
-ax.plot(fitDat[0],fitDat[1],color = "red")
+ax.errorbar(data[0],data[1],fmt="x",yerr=data[2], ecolor = 'black',elinewidth=0.9,capsize=4,capthick=0.9,label="Messdaten")
+ax.plot(fitDat[0],fitDat[1],color = "red",label=fr"fit mit $\chi$={round_errtex(popt[0],np.sqrt(np.diag(perr))[0])}" )
+
+
+
+# Define the formatter function
+def format_func(value, tick_number):
+    return round(value*1e-3,3)
+
+# Set the x-axis formatter
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_func))
+ax.set_xlabel(X_LABEL)
+ax.set_ylabel(Y_LABEL)
+ax.set_xlim(X_START,X_END)
+ax.set_ylim(Y_START,Y_END)
+ax.legend()
+
+
+ax.xaxis.set_major_locator(MultipleLocator(X_MAJOR_TICK))
+ax.xaxis.set_minor_locator(MultipleLocator(X_MINOR_TICK))
+ax.yaxis.set_major_locator(MultipleLocator(Y_MAJOR_TICK))
+ax.yaxis.set_minor_locator(MultipleLocator(Y_MINOR_TICK))
 
 plt.savefig(SAVE_AS)
 plt.show()
