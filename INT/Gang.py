fd4f6aeb5a53d7f0332998474c08a0de217a47de
brouwerb <benedictbrouwer@gmail.com>
2023-03-09 18:25:28 +0100

    luft

--- INT/Gang.py
+++ INT/Gang.py
@@ -1,5 +1,10 @@
 from uncertainties.unumpy import *
+from uncertainties import ufloat
 
 data = uarray([17.00, 33.30 - 17.00, 17.00, 33.40 - 17.00, 15.20, 32.60 - 15.29, 17.20, 34.00 - 17.20], 0.21)
+l = ufloat(632.8, 0.1)*1e-9
 
-50/data
+g = l/(2*data)
+
+gav = sum(g)/len(g)
+print(gav)
