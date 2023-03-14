057865a2b33cb342b0ee6435ff87f304885998df
brouwerb <benedictbrouwer@gmail.com>
2023-03-14 14:53:18 +0100

    int wieder

--- INT/Gang.py
+++ INT/Gang.py
@@ -4,7 +4,7 @@ from uncertainties import ufloat
 data = uarray([17.00, 33.30 - 17.00, 17.00, 33.40 - 17.00, 15.20, 32.60 - 15.29, 17.20, 34.00 - 17.20], 0.21)
 l = ufloat(632.8, 0.1)*1e-9
 
-g = l/(2*data)
+g = 2500*l/(2*data)
 
 gav = sum(g)/len(g)
 print(gav)
