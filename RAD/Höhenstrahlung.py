57066e4900990f71e952bb7ec8222374926e1c16
Jascha Fricker <frickerjascha@gmail.com>
2023-03-14 18:02:14 +0100

    jh

--- RAD/Höhenstrahlung.py
+++ RAD/Höhenstrahlung.py
@@ -10,27 +10,32 @@ import matplotlib.ticker as ticker
 
 
 COLOR_STYLE = ["red","green","blue","orange","violet","darkgreen"]
-Y_LABEL = r"Hits in 400s"
-X_LABEL = r"Kanäle"
+Y_LABEL = r"Hits in 2h"
+X_LABEL = r"Energie in MeV"
 X_START =0 
 Y_START =0
-X_END = 100
-Y_END = 20000
+X_END = 50000
+Y_END = 35
 
-X_MAJOR_TICK = 20
-Y_MAJOR_TICK =2000
-X_MINOR_TICK = 5
-Y_MINOR_TICK = 500
-SAVE_AS = "./RAD/plots/Cobalt-Höhen.pdf"
+X_MAJOR_TICK = 10000
+Y_MAJOR_TICK = 5
+X_MINOR_TICK = 2000
+Y_MINOR_TICK = 1
+SAVE_AS = "./RAD/plots/Höhenstrahlung.pdf"
 
 path_ = "./RAD/RAD-Cosmic.xls"
 
-data = getTableFromCells("A5","B1028",path_,"Höhenstrahlung")
+data = getTableFromCells("C5","D1028",path_,"Höhenstrahlung")
+
+def calEnergie (Kanal):
+    a = 48.8
+    b = 8
+    return a*Kanal+b
+
+data = [calEnergie(np.array(data[0][100:])),data[1][100:]]
 
 
 
-
-peaks = [24.0,27.0,51.2]
 
 
 
@@ -40,22 +45,22 @@ fig, ax = plt.subplots()
 
 ax.grid()
 
-ax.plot(data[0],data[1],color= COLOR_STYLE[0])
-
-for i,I in enumerate(peaks):
-    
-    ax.axvline(I,color = "black",linewidth=0.8)
-
-
+ax.plot(data[0],data[1],color= COLOR_STYLE[0],label = "Höhenstrahlung")
 
 
 ax.set_xlabel(X_LABEL)
 ax.set_ylabel(Y_LABEL)
 ax.set_xlim(X_START,X_END)
 ax.set_ylim(Y_START,Y_END)
-ax.legend(("Cobalt","peaks"))
+ax.legend()
 
 
+# Define the formatter function
+def format_func(value, tick_number):
+    return round(value*1e-3,3)
+
+# Set the x-axis formatter
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_func))
 ax.xaxis.set_major_locator(MultipleLocator(X_MAJOR_TICK))
 ax.xaxis.set_minor_locator(MultipleLocator(X_MINOR_TICK))
 ax.yaxis.set_major_locator(MultipleLocator(Y_MAJOR_TICK))
