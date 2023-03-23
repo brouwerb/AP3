37fc29d66a6d2f59f0c81325d065c8c8f4d0cf88
Jascha Fricker <frickerjascha@gmail.com>
2023-03-23 14:23:43 +0100

    as

--- BUB/einzelspalt.py
+++ BUB/einzelspalt.py
@@ -12,55 +12,63 @@ from uncertainties.unumpy import *
 from uncertainties import ufloat
 import numpy as np
 
+uncertanty = ufloat(5,0.1)/(ufloat(100,0.1)- ufloat(10,0.1))
+print(uncertanty)
 
-
-Y_LABEL = r"Ordnung der Minima"
-X_LABEL = r"Winkel \alpha in °"
-X_START =0 
+X_LABEL = r"Ordnung der Minima"
+Y_LABEL = r"Verhältnis $\frac{s}{l}$"
+X_START =0.5
 Y_START =0
-X_END = 3e-4+1
-Y_END = 120000
+X_END = 5.5
+Y_END = 0.02
 
-X_MAJOR_TICK = 0.5e-4
-Y_MAJOR_TICK =20000
+X_MAJOR_TICK = 1
+Y_MAJOR_TICK = 0.005
 X_MINOR_TICK = 0.1e-4
-Y_MINOR_TICK = 5000
+Y_MINOR_TICK = 0.001
 SAVE_AS = "./BUB/plots/einzelspalt.pdf"
 
 path_ = "./BUB/BUB.xls"
 
 data = getTableFromCells("C24","E29",path_,"Spalt")
-
+print(data)
 plotData = []
+fitData = [[],[]]
 for i in data:
-    plotData.append([[1,2,3,4,5],[i[j+1] /i[0] for j in range(len(i)-1)]])
+    plotData.append([[1,2,3,4,5],[(i[j+1] /i[0]/2 )for j in range(len(i)-1)]])
+    fitData[0].extend([1,2,3,4,5])
+    fitData[1].extend([(i[j+1] /i[0]/2 )for j in range(len(i)-1)])
 
 print(plotData)
 #---------------  fit
 # T = 22.7+273.15
-# def func(n,x,c):
-#     return (n-c)*T*1/x
-# def funcArr(n,arr):
-#     return func(n,arr[0],arr[1])
+def func(x,a,b):
+    return a * x +b
+def funcArr(n,arr):
+    return func(n,arr[0],arr[1])
 
 
 # popt,perr = optimize.curve_fit(func,data[0],data[1],p0=[1e-12,0])
 # print(popt,np.sqrt(np.diag(perr)))
 # fitDat =genDataFromFunktion(1000,0,3e-4,popt,funcArr)
+fig, ax = plt.subplots()
+
+l = [189.9,121.6,62.4]
+ax.grid()
+for i,I in enumerate(plotData):
+    
+    ax.errorbar(I[0],I[1],fmt="o",yerr=unumpy.std_devs(uncertanty),elinewidth=0.9,capsize=4,capthick=0.9,ecolor = "black",label = str(l[i]) + " cm")
 
 
-#ax.scatter(data[0],data[1],s=15)
-# ax.errorbar(np.array(data[0])-popt[1]+1,data[1],fmt="x",yerr=data[2], ecolor = 'black',elinewidth=0.9,capsize=4,capthick=0.9,label="Messdaten")
-# ax.plot(np.array(fitDat[0])-popt[1]+1,fitDat[1],color = "red",label=fr"fit mit $\chi$={round_errtex(popt[0],np.sqrt(np.diag(perr))[0])}" )
+popt,perr = optimize.curve_fit(func,fitData[0],fitData[1])
+print(popt,np.sqrt(np.diag(perr)))
+fitDat =genDataFromFunktion(10,X_START,X_END,popt,funcArr)
+ax.plot(fitDat[0],fitDat[1],label=fr"Ausgleichsgerade")
 
 
-
-# Define the formatter function
-def format_func(value, tick_number):
-    return round(value*1e-3,3)
 
 # Set the x-axis formatter
-ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_func))
+# ax.yaxis.set_major_formatter(ticker.FuncFormatter(format_func))
 ax.set_xlabel(X_LABEL)
 ax.set_ylabel(Y_LABEL)
 ax.set_xlim(X_START,X_END)
@@ -69,9 +77,14 @@ ax.legend()
 
 
 ax.xaxis.set_major_locator(MultipleLocator(X_MAJOR_TICK))
-ax.xaxis.set_minor_locator(MultipleLocator(X_MINOR_TICK))
 ax.yaxis.set_major_locator(MultipleLocator(Y_MAJOR_TICK))
-ax.yaxis.set_minor_locator(MultipleLocator(Y_MINOR_TICK))
+
 
 plt.savefig(SAVE_AS)
 plt.show()
+
+
+sl = ufloat(3.64e-3,0.13e-3)
+lam = ufloat(532e-9,1e-9)
+res = lam/sl
+print(res)
